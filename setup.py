f70937e278505b5a5537a488dfc81c05257a7970
Peter Hart <pjh6654@rit.edu>
2020-03-01 01:50:15 -0500

    updated setup.py for src layout

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pjh6654/wrap-odr",
-    packages=setuptools.find_packages(),
+    package_dir={'labfit': 'src'},
+    packages=setuptools.find_packages(where='src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
